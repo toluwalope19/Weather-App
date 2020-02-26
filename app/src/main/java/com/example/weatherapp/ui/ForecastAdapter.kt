4d15b701cae99cfff4cdb156ef9e3cd68311f30c
ayodeletolulope18 <ayodeletolulope18@gmail.com>
2020-02-26 17:17:10 +0100

    set up recycler view

--- app/src/main/java/com/example/weatherapp/ui/ForecastAdapter.kt
+++ app/src/main/java/com/example/weatherapp/ui/ForecastAdapter.kt
@@ -1,4 +1,42 @@
 package com.example.weatherapp.ui
 
-class ForecastAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.weatherapp.databinding.ForecastTabBinding
+import com.example.weatherapp.model.ForecastWeather
+
+class ForecastAdapter(): RecyclerView.Adapter<ForecastAdapter.ForecastHolder>() {
+
+    val weathers = listOf<ForecastWeather>()
+
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ForecastHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ForecastTabBinding.inflate(inflater)
+
+        return ForecastHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return weathers.size
+    }
+
+    override fun onBindViewHolder(holder: ForecastHolder, position: Int) {
+
+            val weather = weathers[position]
+            holder.bind(weather)
+    }
+
+    class ForecastHolder(private val binding: ForecastTabBinding): RecyclerView.ViewHolder(binding.root) {
+
+            fun bind(item: ForecastWeather){
+                binding.weather = item
+                binding.executePendingBindings()
+            }
+
+    }
 }
